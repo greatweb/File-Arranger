db348842b10cbd7b283ffb234ed5c3ca4da03aad
greatweb <maximredmond@gmail.com>
2020-04-15 23:54:41 +0100

    Added .json files for the file extensions.

--- fileArranger.py
+++ fileArranger.py
@@ -1,13 +1,21 @@
 import os
+import json
+import string
 
-folders = ["Applications", "Documents", "Torrents", "Zip Folders", "Images", "Videos", "Audio Files", "Other Files"]
+folders = ["Applications", "Documents", "Torrents", "Compressed Folders", "Images", "Videos", "Audio Files", "Other Files", "Installers"]
+execFileExtensions = json.loads(open('executableFileExtensions.json').read())
+videoFileExtensions = json.loads(open('videoFileExtensions.json').read())
+audioFileExtensions = json.loads(open('audioFileExtensions.json').read())
+compressedFileExtensions = json.loads(open('compressedFileExtensions.json').read())
+imageFileExtensions = json.loads(open('imageFileExtensions.json').read())
+documentFileExtensions = json.loads(open('documentFileExtensions.json').read())
 
 bool1 = True
 bool2 = True
 i = 0
 
-while bool1 == True:
-    while bool2 == True:
+while bool1:
+    while bool2:
         downloadDirectoryInput = input('Paste the path of the folder that you want to arrange: ')
         list(downloadDirectoryInput)
         downloadDirectoryInput = downloadDirectoryInput.replace("\\", "/")
@@ -44,19 +52,21 @@ while bool1 == True:
             file_extension_splitted = file_extension.strip('.')
             if any(p == DOWNLOAD_DIRECTORY + a for a in folders):
                 continue
-            if any(file_extension_splitted.lower() == a for a in ["exe", "msi"]):
+            if any(file_extension.upper() == b for b in execFileExtensions):
                 os.rename(p, DOWNLOAD_DIRECTORY + "Applications/" + os.path.basename(p))
-            elif any(file_extension_splitted.lower() == a for a in ["pdf", "txt", "docx", "pptx", "ppt", "xlsx", "xls"]):
+            elif any(file_extension.upper() == c for c in documentFileExtensions):
                 os.rename(p, DOWNLOAD_DIRECTORY + "Documents/" + os.path.basename(p))
-            elif file_extension_splitted.lower() == "torrent":
+            elif file_extension.upper() == ".TORRENT":
                 os.rename(p, DOWNLOAD_DIRECTORY + "Torrents/" + os.path.basename(p))
-            elif any(file_extension_splitted.lower() == a for a in ["rar", "zip", "7z"]):
-                os.rename(p, DOWNLOAD_DIRECTORY + "Zip Folders/" + os.path.basename(p))
-            elif any(file_extension_splitted.lower() == a for a in ["jpg", "jpeg", "png", "psd"]):
+            elif any(file_extension.upper() == d for d in compressedFileExtensions):
+                os.rename(p, DOWNLOAD_DIRECTORY + "Compressed Folders/" + os.path.basename(p))
+            elif any(file_extension.upper() == e for e in imageFileExtensions):
                 os.rename(p, DOWNLOAD_DIRECTORY + "Images/" + os.path.basename(p))
-            elif any(file_extension_splitted.lower() == a for a in ["mov", "mp4"]):
+            elif file_extension.upper() == ".MSI":
+                os.rename(p, DOWNLOAD_DIRECTORY + "Installers/" + os.path.basename(p))
+            elif any(file_extension.upper() == f for f in videoFileExtensions):
                 os.rename(p, DOWNLOAD_DIRECTORY + "Videos/" + os.path.basename(p))
-            elif file_extension_splitted.lower() == "mp3":
+            elif any(file_extension.upper() == g for g in audioFileExtensions):
                 os.rename(p, DOWNLOAD_DIRECTORY + "Audio Files/" + os.path.basename(p))
             else:
                 os.rename(p, DOWNLOAD_DIRECTORY + "Other Files/" + os.path.basename(p))
@@ -67,6 +77,10 @@ while bool1 == True:
     print("Done!", i, "files have been moved!")
 
     for f in folders:
+        print(str(f) , os.stat(f).st_size)
+
+    for f in folders:
         if os.stat(f).st_size == 0:
-            os.rmdir(DOWNLOAD_DIRECTORY + f)
+            os.rmdir(f)
+
     bool1 = False
